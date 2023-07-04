3dfc680bb9e5d728ade08dc233a6cf9c55b23451
Sudarshan519 <sudarshanshrestha519@gmail.com>
2023-07-04 18:04:56 +0545

    repo push

--- checkbox/script.js
+++ checkbox/script.js
@@ -3,6 +3,9 @@ var x = document.getElementById("container");
 var checkboxes = x.querySelectorAll("input[type='checkbox']");
 var selectall = document.getElementById("option-all")
 
+
+
+
 console.log(checkboxes);
 var allchecked = false
 var isjs = false
@@ -32,16 +35,16 @@ function myCreateFunction() {
     var table = document.getElementById("myTable");
     var body = `
         <tr>
-            <th>Check AllA<input type="checkbox" value="" id="option-all4" onchange="checkAll(this)"></th>
-            <th>Check All B<input type="checkbox" value="" id="option-all5" onchange="checkAll(this)"></th>
-            <th>Check All C<input type="checkbox" value="" id="option-all6" onchange="checkAll(this)"></th>
+            <th>Check AllA<input type="checkbox" value="" id="option-ll4" onchange="checkAll(this)"></th>
+            <th>Check All B<input type="checkbox" value="" id="option-ll5" onchange="checkAll(this)"></th>
+            <th>Check All C<input type="checkbox" value="" id="option-ll6" onchange="checkAll(this)"></th>
         </tr>
       
 
 
     `
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 5; i++) {
         body += ` <tr>
         <td>   <input type="checkbox" id="option-a${i}">
         <label for="option-a${i}">${i}</label></td>
@@ -63,13 +66,112 @@ function myCreateFunction() {
 }
 myCreateFunction()
 
-
-var acheckbox = document.getElementById("option-all4")
-var bchecbox = document.getElementById("option-all5")
-var cCheckbox = document.getElementById("option-all6")
+var acheckbox = document.getElementById("option-ll4")
+var bchecbox = document.getElementById("option-ll5")
+var cCheckbox = document.getElementById("option-ll6")
 
 var checkboxes = x.querySelectorAll("input[type='checkbox']");
-var optionalist=document.querySelectorAll('[id*="option-a"]');
+var optionalist = document.querySelectorAll('[id*="option-a"]');
+var optionblist = document.querySelectorAll('[id*="option-b"]');
+var optionclist = document.querySelectorAll('[id*="option-c"]');
+var allAchecked = true;
+
+
+acheckbox.addEventListener('change', (event) => {
+    console.log(acheckbox.checked)
+    optionalist.forEach(function (checkbox) {
+        checkbox.checked = acheckbox.checked
+    })
+})
+
+
+bchecbox.addEventListener('change', (event) => {
+    console.log(bchecbox.checked)
+    optionblist.forEach(function (checkbox) {
+        checkbox.checked = bchecbox.checked
+    })
+})
+
+cCheckbox.addEventListener('change', (event) => {
+    console.log(cCheckbox.checked)
+    optionclist.forEach(function (checkbox) {
+        checkbox.checked = cCheckbox.checked
+    })
+})
+
+optionclist.forEach(function (checkbox) {
+
+    checkbox.addEventListener('change', (event) => {
+        var hasFalse = false
+        allchecked = true
+        optionclist.forEach(function (checkbox) {
+            if (checkbox.checked == false) {
+                hasFalse = true
+                console.log("has false")
+            } else {
+                allAchecked = true
+            }
+        })
+        if (hasFalse == true) {
+            cCheckbox.checked = false
+        }
+        else {
+            console.log("all checked")
+            cCheckbox.checked = true
+        }
+    })
+})
+
+
+
+
+optionblist.forEach(function (checkbox) {
+
+    checkbox.addEventListener('change', (event) => {
+        var hasFalse = false
+        allchecked = true
+        optionblist.forEach(function (checkbox) {
+            if (checkbox.checked == false) {
+                hasFalse = true
+                console.log("has false")
+            } else {
+                allAchecked = true
+            }
+        })
+        if (hasFalse == true) {
+            bchecbox.checked = false
+        }
+        else {
+            console.log("all checked")
+            bchecbox.checked = true
+        }
+    })
+})
+
+
+
+optionalist.forEach(function (checkbox) {
+
+    checkbox.addEventListener('change', (event) => {
+        var hasFalse = false
+        allchecked = true
+        optionalist.forEach(function (checkbox) {
+            if (checkbox.checked == false) {
+                hasFalse = true
+                console.log("has false")
+            } else {
+                allAchecked = true
+            }
+        })
+        if (hasFalse == true) {
+            acheckbox.checked = false
+        }
+        else {
+            console.log("all checked")
+            acheckbox.checked = true
+        }
+    })
+})
 // checkboxes.forEach(function (e) {
 //     e.addEventListener('change', (event) => {
 //         if (event.currentTarget.checked) {
